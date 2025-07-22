30175b719ab959558a76b11e5c0a0bf815fa44db
Prajwal H S <prajwalhs4india@gmail.com>
2025-07-22 12:25:35 +0530

    added  favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,25 @@ import "./globals.css";
 import NavBar from "./../components/NavBar/NavBar";
 import Footer from "./../components/Footer/Footer";
 import Whatsapp from "./../components/Whatsapp/Whatsapp";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  <head>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-    />
-    <link rel="icon" type="image/png" href="./favicon.png" />
-  </head>
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link rel="icon" type="image/png" href="./favicon.png" />
+      </head>
       <body>
-        <NavBar /> {children}
+        <NavBar />
+        {children}
         <Footer />
         <Whatsapp />
       </body>
