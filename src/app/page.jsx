f742c4b71e6ef4d5fa1b73bd4999cafb55ad72e8
Prajwal H S <prajwalhs4india@gmail.com>
2025-07-23 21:13:01 +0530

    changes1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,9 @@
 import Video from "./../components/HomePage/Video/Video";
 import Services from "./../components/HomePage/Services/Services";
-import Members from "./../components/HomePage/Members/Members";
-import OurSalon from "./../components/HomePage/OurSalon/OurSalon";
-import InstaFeed from "./../components/HomePage/InstaFeed/InstaFeed";
 import ServiceWhy from "./../components/HomePage/ServiceWhy/ServiceWhy";
 import Partners from "../components/HomePage/FromCourse/FromCourse";
 import HelpNearYou from "./../components/HomePage/Help/HelpNearYou";
 import TextLocReviews from "./../components/HomePage/TextLocReviews/TextLocReviews";
-import About from "./../components/HomePage/About/About";
 import Nurture from "./../components/HomePage/Nurture/Nurture";
 import Faculty from "./../components/HomePage/Faculty/Faculty";
 import FormHeader from './../components/Form/FormHeader';
@@ -20,13 +16,8 @@ export default function Home() {
       <Services />
       <Faculty />
       <Partners />
-      {/* <About /> */}
       <Nurture />
       <ServiceWhy />
-      {/* <Members /> */}
-      {/* <OurSalon /> */}
-      {/* <InstaFeed /> */}
-      {/* <HelpNearYou /> */}
       <Testimonial/>
       <TextLocReviews />
     </div>
